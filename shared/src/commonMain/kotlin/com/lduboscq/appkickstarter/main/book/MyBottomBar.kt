923add5cef9a5e32cc9e013b526b87d32c9ee31f
Dongguo WU <david.dong.guo@gmail.com>
2023-06-18 09:05:21 -0400

    use badage on shopping cart icon

--- shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/main/book/MyBottomBar.kt
+++ shared/src/commonMain/kotlin/com/lduboscq/appkickstarter/main/book/MyBottomBar.kt
@@ -1,35 +1,53 @@
 package com.lduboscq.appkickstarter.main.book
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.lduboscq.appkickstarter.model.User
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyBottomBar(quantity: Int, currentScreen: Route) {
     val navigator = LocalNavigator.currentOrThrow
     BottomAppBar(
-        backgroundColor = MaterialTheme.colorScheme.secondaryContainer,
         contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
-        content = {
-
-            // Home Icon
+        containerColor = MaterialTheme.colorScheme.surface,
+        tonalElevation = 36.dp,
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Check, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = "Localized description",
+                )
+            }
             IconButton(
                 onClick = {
                     if (currentScreen !is Route.Home) {
-                        val user = User("Donggu")
+                        val user = User("Dongguo")
                         navigator.push(screenRouter(Route.Home(user)))
                     }
 
@@ -37,28 +55,43 @@ fun MyBottomBar(quantity: Int, currentScreen: Route) {
             ) {
                 Icon(
                     imageVector = Icons.Outlined.Home,
-                    contentDescription = "Back Home"
+                    contentDescription = "Back Home",
                 )
             }
-
-            Spacer(modifier = Modifier.width(30.dp))
-            // Shopping cart icon
-            IconButton(
+        },
+        floatingActionButton = {
+            FloatingActionButton(
                 onClick = {
                     if (currentScreen !is Route.ShoppingCart) {
                         navigator.push(screenRouter(Route.ShoppingCart(quantity)))
                     }
+                },
+                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+                content = {
+                    BadgedBox(
+                        badge = {
+                            Badge {
+                                if (quantity >= 1) {
+                                    Text(
+                                        quantity.toString(),
+                                        modifier = Modifier.semantics {
+                                            contentDescription = "$quantity books in shopping cart"
+                                        },
+                                        fontSize = 16.sp
+                                    )
+                                }
+                            }
+                        }) {
+                        Icon(
+                            Icons.Filled.ShoppingCart,
+                            contentDescription = "Display the quantity of books in shopping cart"
+                        )
+                    }
                 }
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.ShoppingCart,
-                    contentDescription = "shopping cart"
-                )
-            }
 
-            Text(
-                text = quantity.toString()
             )
         }
     )
 }
+
